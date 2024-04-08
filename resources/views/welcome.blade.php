f777ff00caf5c876b88f9bfe20b31b81efea8317
VictorBaleroni <victorcostabaleroni0@gmail.com>
2024-04-08 19:24:41 -0300

    dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,39 +3,28 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <title>Biblioteca Online</title>
 
     </head>
     <body>
         
-                        @if (Route::has('login'))
-                            <nav>
-                                @auth
-                                    <a
-                                        href="{{ url('/dashboard') }}"
-                                    >
-                                        Dashboard
-                                    </a>
-                                @else
-                                    <a
-                                        href="{{ route('login') }}"
-                                    >
-                                        Log-in
-                                    </a>
-
-                                    @if (Route::has('register'))
-                                        <a
-                                            href="{{ route('register') }}"
-                                        >
-                                            Register
-                                        </a>
-                                    @endif
-                                @endauth
-                            </nav>
-                        @endif
-                   
+        @if (Route::has('login'))
+        <nav>
+        @auth
+            <a href="{{ url('/dashboard') }}">Dashboard</a>
+        
+        @else
+            <a href="{{ route('login') }}">Log-in</a>
+        
+        @if (Route::has('register'))
+            <a href="{{ route('register') }}">Register</a>
+        
+        @endif
+        @endauth
+        
+        </nav>
+        @endif
     </body>
 </html>
